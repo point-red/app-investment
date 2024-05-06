73f46dbadde863478e690e74b0a69aa613bb8f8f
Gunadi Wirawan <gunadi@starworksglobal.com>
2024-05-06 14:40:36 +0800

    Revert "fix: fix deposit"
    This reverts commit d616704096410a17c4c27312bec75a593b823dea.

--- src/views/investment/Main.vue
+++ src/views/investment/Main.vue
@@ -5,10 +5,7 @@
   <!-- BEGIN: Page Layout -->
   <div class="grid grid-cols-12 gap-6 mt-5">
     <router-link
-      v-if="
-        authStore.permissions.includes('deposit.view') ||
-        authStore.permissions.includes('deposit.withdrawal')
-      "
+      v-if="authStore.permissions.includes('deposit.view')"
       :to="{ name: depositNav.home.name }"
       class="intro-y col-span-12 md:col-span-3 lg:col-span-3 box py-10 hover:shadow-xl transition-all duration-500"
     >
