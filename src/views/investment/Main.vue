a221e18e2c6052f45644a24c6a4a460782124610
Gunadi Wirawan <gunadi@starworksglobal.com>
2024-05-06 14:41:51 +0800

    fix: fix deposit

--- src/views/investment/Main.vue
+++ src/views/investment/Main.vue
@@ -5,7 +5,10 @@
   <!-- BEGIN: Page Layout -->
   <div class="grid grid-cols-12 gap-6 mt-5">
     <router-link
-      v-if="authStore.permissions.includes('deposit.view')"
+      v-if="
+        authStore.permissions.includes('deposit.view') ||
+        authStore.permissions.includes('deposit.withdrawal')
+      "
       :to="{ name: depositNav.home.name }"
       class="intro-y col-span-12 md:col-span-3 lg:col-span-3 box py-10 hover:shadow-xl transition-all duration-500"
     >
